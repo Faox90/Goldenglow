da359e38ac27255dbfe391c4ae37fa2d82f4c4de
Faox90 <eaglefk@hotmail.com>
2024-08-05 22:48:22 +0200

    Add files via upload

---
+++ Goldenglow/assets/map/paths.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="paths" tilewidth="16" tileheight="16" tilecount="64" columns="4">
+ <image source="paths.png" width="64" height="256"/>
+</tileset>
